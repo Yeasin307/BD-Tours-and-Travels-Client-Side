1410f0a2f2d170ec8e425a50b4508cbd7e51ad7a
Yeasin <yeasinhossen7972@gmail.com>
2021-10-31 22:24:33 +0600

    final

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCM6isGBFy2L-W-WFpr6_HBdRZgBPzNk2k",
+    authDomain: "bd-tours-and-travels.firebaseapp.com",
+    projectId: "bd-tours-and-travels",
+    storageBucket: "bd-tours-and-travels.appspot.com",
+    messagingSenderId: "700883606391",
+    appId: "1:700883606391:web:54b024cabed282c19ddf72"
+};
+
+export default firebaseConfig;
